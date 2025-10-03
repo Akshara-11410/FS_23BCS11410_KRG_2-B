5adc61556f138c196afc2367b9f313a8bcf4cef8
Akshara Chauhan <23BCS11410@cuchd.in>
2025-10-03 16:00:13 +0530

    Add files via upload

---
+++ LabMst/Form_App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./FormWithTable";  
+import "./index.css"; 
+
+function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
