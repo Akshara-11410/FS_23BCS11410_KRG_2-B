e3b6c84619447a1d7943dfac621aeba163966a75
Akshara Chauhan <23BCS11410@cuchd.in>
2025-10-03 21:43:06 +0530

    Add files via upload

---
+++ LabMst/Router_app/src/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">About Page</h1>
+      <p>I am a CSE student at Chandigarh University.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ LabMst/Router_app/src/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Contact Page</h1>
+      <p>my email id is 23bcs11410@cuchd.in</p>
+    </div>
+  );
+}
+
+export default Contact;
